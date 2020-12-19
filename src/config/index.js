048361fede318fb6176dad87d6360b55d5a0546d
Lijomon C John <lijomon@promasoft.net>
2020-12-19 13:34:56 +0530

    Configurations modifeies with  uniest credentials

--- src/config/index.js
+++ src/config/index.js
@@ -1,34 +1,34 @@
 export default {
-  username: 'ascherbachenko@cs-cart.com',
-  // API KEY
-  apiKey: '62f5a9c133da276c516cc7f491f8a644',
-  // API URL
-  baseUrl: 'http://mobile.mve.demo.cs-cart.com/api/4.0/',
-  // SITE URL
-  siteUrl: 'http://mobile.mve.demo.cs-cart.com/',
-  // SHOP NAME
-  shopName: 'CS-Cart Mobile',
-  // SHOP DEFAULT LAYOUT ID
-  layoutId: 2, // Default www.mobile.demo.cs-cart.com layout id
-  // VERSION MVE OR ULT
-  version: 'MVE',
-  // Enable push notifications
-  pushNotifications: true,
-  pushNotificationChannelId: 'cscart.default',
-  pushNotificationsColor: '#26323e',
-  // Demo app features
-  demo: true,
-  demoUsername: 'acmecorp@example.com',
-  demoPassword: 'vendor',
+	username: 'ashil@promatas.com',
+	// API KEY
+	apiKey: '87e0af6ce7b60ef8994e439b3a85622a',
+	// API URL
+	baseUrl: 'http://3.7.66.12/ptz/api/4.0/',
+	// SITE URL
+	siteUrl: 'http://3.7.66.12/ptz/index.php',
+	// SHOP NAME
+	shopName: 'UNIEST',
+	// SHOP DEFAULT LAYOUT ID
+	layoutId: 4, // Default www.mobile.demo.cs-cart.com layout id
+	// VERSION MVE OR ULT
+	version: 'MVE',
+	// Enable push notifications
+	pushNotifications: true,
+	pushNotificationChannelId: 'cscart.default',
+	pushNotificationsColor: '#26323e',
+	// Demo app features
+	demo: true,
+	demoUsername: 'lijomon92@gmail.com',
+	demoPassword: 'qwerty',
 
-  // Apple pay payments
-  applePay: true,
-  applePayMerchantIdentifier: 'merchant.com.cscart',
-  applePayMerchantName: 'CSCartmultivendor',
-  applePaySupportedNetworks: ['visa'],
+	// Apple pay payments
+	applePay: true,
+	applePayMerchantIdentifier: 'merchant.com.cscart',
+	applePayMerchantName: 'CSCartmultivendor',
+	applePaySupportedNetworks: ['visa'],
 
-  // Google pay payments
-  googlePay: true,
-  googlePayApiKey: 'api-key',
-  googlePaySupportedNetworks: ['visa'],
+	// Google pay payments
+	googlePay: true,
+	googlePayApiKey: 'api-key',
+	googlePaySupportedNetworks: ['visa'],
 };
